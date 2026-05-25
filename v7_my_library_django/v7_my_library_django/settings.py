62d658e43470e5409b4244b28a2556e357f8804b
diego <gaibo.jr.04@gmail.com>
2026-05-25 10:59:50 +0200

    messi libri e authors

--- v7_my_library_django/v7_my_library_django/settings.py
+++ v7_my_library_django/v7_my_library_django/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "commons", 
-    "dashboard"
+    "dashboard",   
+    "authors",
+    "books",
 ]
 
 MIDDLEWARE = [
